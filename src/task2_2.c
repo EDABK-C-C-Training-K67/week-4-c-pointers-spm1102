feb98b212c28d00449573888c769b25dba61d680
spm1102 <dovietdung_hsgs19@hus.edu.vn>
2024-04-24 01:41:03 +0700

    [MODIFY] complete the coding task

--- src/task2_2.c
+++ src/task2_2.c
@@ -1,78 +1,21 @@
 #include <stdio.h>
-#include <math.h>
 
-void reverse(int arr[], int left, int right);
+void floatToBinary(float num) {
+  unsigned int* ptr = (unsigned int*)&num;
+  unsigned int binary = *ptr;
+  unsigned int mask = 1 << 31;
+  for (int i = 0; i < 32; i++) {
+      printf("%d", (binary & mask) ? 1 : 0);
+      binary <<= 1;
+      if ((i + 1) % 8 == 0);
+    }
 
-void real_to_bin(float num);
-
+    printf("\n");
+}
 
 int main() {
-	float num;
-    scanf("%f", &num);
-    real_to_bin(num);
-
-    return 0;
-}
-
-void reverse(int arr[], int left, int right){
-    int temp, i;
-    for(i = 0; i < (int)((right -left) / 2 + 1); i++){
-        temp = arr[left + i];
-        arr[left + i] = arr[right -i];
-        arr[right - i] = temp;
-    } 
-}
-
-void real_to_bin(float num){
-    int binary[32] = {0};
-    // Sign bit
-    if(num > 0) {
-        binary[0] = 0;
-    }
-    else if(num == 0){
-    	int j;
-    	for(j = 0; j < 32; j++){
-    		printf("%d", binary[j]);
-		}
-		return;
-	}
-    else {
-        binary[0] = 1;
-        num = -num;
-    }
-	
-    int int_part = (int)num, i = 1; 
-    float floating_part = num - (float)int_part;
-    int *ptr = binary;
-    ptr = ptr + 8;
-    while(int_part > 0){
-        *(ptr + i++) = int_part % 2;
-        int_part /= 2;
-    }
-    int cnt = i;
-    reverse(binary, 8, 8 + i - 1 );
-
-    while(floating_part > 0){
-        floating_part *= 2;
-        if(floating_part < 1){
-            *(ptr + i++ - 1) = 0;
-        }
-        else{
-            *(ptr + i++ -1) = 1;
-            floating_part -= 1;
-        }
-    }
-
-    
-    int e = 127 + cnt - 2;
-    i = 1;
-    while(e > 0){
-        binary[i++] = e % 2;
-        e /= 2;
-    }
-    reverse(binary, 1, 8);
-    
-    for(i = 0; i < 32; i++){
-        printf("%d", binary[i]);
-    }
+  float num;
+  scanf("%f", &num);
+  floatToBinary(num);
+  return 0;
 }
--- src/task4_0.c
+++ src/task4_0.c
@@ -1,21 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 #define max_line_length 30
 
-void allocate(char ***arr, int num_lines, int line_length);
+void allocate(char*** arr, int num_lines, int line_length);
+
+
 
 int main(){
-    int num_line = 10, line_length = max_line_length;
+    int num_line = 1, line_length = max_line_length;
     char **arr = NULL;
     allocate(&arr, num_line, line_length);
 
-    for(int i = 0; i < num_line; i++){
-        scanf("%s", arr[i]);
-    }
+    while(1){
+        for(int i = 0; i < num_line; i++){
+            fgets(arr[i], line_length + 1,stdin);
+             if (arr[i][strlen(arr[i]) - 1] == '\n') {
+                arr[i][strlen(arr[i]) - 1] = '\0';
+            }
+        }
 
-    for(int i = 0; i < num_line; i++){
-        printf("\n %s", arr[i]);
+        for(int i = 0; i < num_line; i++){
+            printf("\n%s", arr[i]);
+        }
+        if (strcmp(arr[num_line - 1], "*") == 0) {
+            break;
+        }
+        arr = (char**)realloc(arr, num_line * sizeof(char *));
+        if(arr ==NULL){
+            return 0;
+        }
+        for (int i = 0; i < num_line; i++) {
+            arr[i] = (char*)realloc(arr[i], (line_length + 1) * sizeof(char*));
+            if(arr[i] == NULL){
+                return 0;
+            }
+        }
     }
 
     for(int i = 0; i < num_line; i++){
@@ -25,8 +46,8 @@ int main(){
     return 0;
 }
 
-void allocate(char ***arr, int num_lines, int line_length){
-    *arr = (char **)calloc(num_lines, sizeof(char));
+void allocate(char*** arr, int num_lines, int line_length){
+    *arr = (char**)calloc(num_lines, sizeof(char));
     if(*arr == NULL){
         exit(1);
     }
@@ -37,3 +58,5 @@ void allocate(char ***arr, int num_lines, int line_length){
         }
     }
 }
+
+
